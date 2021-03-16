612ba16caf87159240d6162479fc3308618e7683
Yusuke MURAOKA <yusuke@jbking.org>
2021-03-16 19:43:43 +0900

    simplify

--- logging_exam/logging.py
+++ logging_exam/logging.py
@@ -1,22 +1,5 @@
-import logging
 from logging.config import fileConfig
-from pythonjsonlogger import jsonlogger
-
-
-class CJsonFormatter(jsonlogger.JsonFormatter):
-    pass
-
-
-old_factory = logging.getLogRecordFactory()
-
-
-def record_factory(*args, **kwargs):
-    # print("args", args)
-    # print("kwargs", kwargs)
-    record = old_factory(*args, **kwargs)
-    return record
 
 
 def setup_logging(conf_path):
     fileConfig(conf_path)
-    logging.setLogRecordFactory(record_factory)
